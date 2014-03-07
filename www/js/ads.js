cc4ee937e82620c69f7e9076fef0a2982b2244db
namleclimax <namle.climax@gmail.com>
2014-03-07 09:26:08 +0700

    bro nam's commit

--- www/js/ads.js
+++ www/js/ads.js
@@ -3,25 +3,30 @@ var admod_andoid = "";
 var admod_ios = "";
 var ad_img = "";
 var ad_target = "";
-
+var inmobi_conf;
 $(document).ready(function () {
     $.ajax({
         url: "http://comic.apptruyen.com/ads.php",
         dataType: 'jsonp',
         success: function (data) {
             ad_type = data.ads;
-            if (data.show_ads){
-                $("#maxads").html('<img onclick="location.href=\'' + data.target + '\'" src="' + data.img + '" />');
+			if (ad_type == "admod"){
+				 admod_andoid = data.admod_andoid;
+                 admod_ios = data.admod_ios;
+			}else if (ad_type == "inmobi"){
+				$("#maxads-footer").show();
+				$("#maxads").hide();
+				 var newConfig = {
+					siteid : data.siteid,
+					slot :  data.slot_number,
+					test: false
+					};
+				_inmobi.getNewAd(document.getElementById('inmobile-ad-slot'), newConfig);
+			}else if (ad_type =="maxads"){
+				$("#maxads").html('<img onclick="location.href=\'' + data.target + '\'" src="' + data.img + '" />');
                 $("#maxads-footer").show();
-            } else {
-                if (ad_type == "admob") {
-                    admod_andoid = data.admod_andoid;
-                    admod_ios = data.admod_ios;
-                } else {
-                    ad_img = data.img;
-                    ad_target = data.target;
-                }
-            }
+				$("#inmobile-ad-slot").hide();
+			}
         },
         error: function () {
             alert("Error");
