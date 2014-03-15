b50706a37a89104d4dcf9ab67d02124e8531bbb2
namleclimax <namle.climax@gmail.com>
2014-03-15 10:23:34 +0700

    a

--- www/js/indexeddb.js
+++ www/js/indexeddb.js
@@ -15,7 +15,7 @@ html5rocks.indexedDB.onerror = function (e) {
 //Step 1. Opening the database
 //Step 2. Creating an Object Store
 
-html5rocks.indexedDB.open = function (onSuccessDoWhat, parameters) {
+html5rocks.indexedDB.open = function (callback) {
     var version = 1;
     var request = indexedDB.open("mycomic", version);
 
@@ -40,7 +40,8 @@ html5rocks.indexedDB.open = function (onSuccessDoWhat, parameters) {
         html5rocks.indexedDB.db = e.target.result;
 
         // ONLY ACCESS DB IN CALLBACK WHEN LOADED, OTHERWISE DB WILL BE UNDEFINED
-        onSuccessDo(onSuccessDoWhat, parameters);
+        // wrap callback in (function(){callback(parameter)}); to avoid calling callback
+        callback();
     };
 
     request.onerror = html5rocks.indexedDB.onerror;
@@ -49,88 +50,86 @@ html5rocks.indexedDB.open = function (onSuccessDoWhat, parameters) {
 
 //Step 3. Adding data to an object store
 
-html5rocks.indexedDB.addCurrentlyRead = function (parameters) {
-    var db = html5rocks.indexedDB.db;
-    var trans = db.transaction(["currentlyRead"], "readwrite");
-    var store = trans.objectStore("currentlyRead");
+html5rocks.indexedDB.addCurrentlyRead2 = function (parameters) {
 
-    // search for existed record
-    var getRequest = store.get(parameters.comicid);
+        var db = html5rocks.indexedDB.db;
+        var trans = db.transaction(["currentlyRead"], "readwrite");
+        var store = trans.objectStore("currentlyRead");
 
-    getRequest.onerror = html5rocks.indexedDB.onerror;
+        // search for existed record
+        var getRequest = store.get(parameters.comicid);
 
-    getRequest.onsuccess = function (e) {
-        // Get the old value that we want to update
-        var data = getRequest.result;
+        getRequest.onerror = html5rocks.indexedDB.onerror;
 
-        // if no record found, create new
-        if(typeof data === 'undefined') data = {};
+        getRequest.onsuccess = function (e) {
+            // Get the old value that we want to update
+            var data = getRequest.result;
 
-        // only update value not undefined into database
-        for (var key in parameters) {
-            if (parameters.hasOwnProperty(key)) {
-                data[key] = parameters[key];
+            // if no record found, create new
+            if(typeof data === 'undefined') data = {};
+
+            // only update value not undefined into database
+            for (var key in parameters) {
+                if (parameters.hasOwnProperty(key)) {
+                    data[key] = parameters[key];
+                }
             }
-        }
 
-        // insert/update DB
-        var request = store.put(data);
+            // insert/update DB
+            var request = store.put(data);
 
-        //request.onsuccess = function(e) {};
-        request.onerror = html5rocks.indexedDB.onerror;
-    };
+            //request.onsuccess = function(e) {};
+            request.onerror = html5rocks.indexedDB.onerror;
+        };
 };
 
+html5rocks.indexedDB.addCurrentlyRead = function(parameters){
+
+    // open DB first, because indexdb is async listing will be in callback
+    html5rocks.indexedDB.open(function(){
+        html5rocks.indexedDB.addCurrentlyRead2(parameters);
+    });
+};
 
 //Step 4. Querying the data in a store.
 
 html5rocks.indexedDB.listCurrentlyRead = function () {
 
-    var db = html5rocks.indexedDB.db;
-    var trans = db.transaction(["currentlyRead"], "readwrite");
-    var store = trans.objectStore("currentlyRead");
+    // open DB first, because indexdb is async listing will be in callback
+    html5rocks.indexedDB.open(function(){
+        var db = html5rocks.indexedDB.db;
+        var trans = db.transaction(["currentlyRead"], "readwrite");
+        var store = trans.objectStore("currentlyRead");
 
-    // Get everything in the store;
-    var keyRange = IDBKeyRange.lowerBound(0);
-    var cursorRequest = store.openCursor(keyRange);
+        // Get everything in the store;
+        var keyRange = IDBKeyRange.lowerBound(0);
+        var cursorRequest = store.openCursor(keyRange);
 
-    cursorRequest.onsuccess = function (e) {
-        var result = e.target.result;
-        if (!!result == false)
-            return;
+        cursorRequest.onsuccess = function (e) {
+            var result = e.target.result;
+            if (!!result == false)
+                return;
 
-        // actual render code
-        var row = result.value;
+            // actual render code
+            var row = result.value;
 
-        // don't display comic without chapter
-        if (row.chapid != undefined && row.chapname != undefined) {
-            $("#table-ds-truyen").append('<tr onclick="location.href=\'view.html?id=' + row.chapid + '&story=' +
-                row.comicid + '\'" ><td>' + row.comicname + '</td><td>' + row.chapname + '</td></tr>');
-        }
+            // don't display comic without chapter
+            if (row.comicname != undefined && row.chapname != undefined) {
+                $("#table-ds-truyen").append('<tr onclick="gotoView(' + row.chapid + ' ,' +row.comicid + ');" ><td>' +
+                    row.comicname + '</td><td>' + row.chapname + '</td></tr>');
+            }
 
-        // next row
-        result.continue();
-    };
+            // next row
+            result.continue();
+        };
 
-    // hide loading screen
-    $(".loading").hide();
+        // hide loading screen
+        $(".loading").hide();
 
-    cursorRequest.onerror = html5rocks.indexedDB.onerror;
+        cursorRequest.onerror = html5rocks.indexedDB.onerror;
+    });
 };
 
 
 // enable debugging with websql shim, must include shim before this file
 window.shimIndexedDB.__debug(true);
-
-
-// ONLY ACCESS DB IN CALLBACK WHEN LOADED, OTHERWISE DB WILL BE UNDEFINED
-function onSuccessDo(onSuccessDoWhat, parameters) {
-    switch (onSuccessDoWhat) {
-        case 'list':
-            html5rocks.indexedDB.listCurrentlyRead();
-            break;
-        case 'add':
-            html5rocks.indexedDB.addCurrentlyRead(parameters);
-            break;
-    }
-}
---
+++ www/js/my_comic.js
@@ -0,0 +1,7 @@
+function gotoView(chapterid, comicid){
+    localStorage.setItem('chapterid', chapterid);
+    localStorage.setItem('comicid', comicid);
+    window.location = 'view.html';
+}
+
+html5rocks.indexedDB.listCurrentlyRead();
